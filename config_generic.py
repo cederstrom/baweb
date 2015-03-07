267fc5df668136e7ac6ef8792d66756d3ff7781d
Andreas Cederström <andreas@andreassmacbook.lan>
2015-03-07 22:19:29 +0100

    Moved SECRET_KEY from config_generic.py to config.py

--- config_generic.py
+++ config_generic.py
@@ -4,6 +4,5 @@ import os
 WTF_CSRF_ENABLED = True
 
 # SQLAlchemy
-SECRET_KEY = 'you-will-never-guess'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
