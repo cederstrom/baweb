a019861ba8f460360cc567e69ea5dd1a6950c0e5
cederstrom <andreas.cederstrom@gmail.com>
2016-03-13 12:39:52 +0100

    Update config_generic.py

--- config_generic.py
+++ config_generic.py
@@ -9,13 +9,13 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 
 FLUMRIDE = {
-    'SUBMIT_OPEN': parser.parse("2015-03-26 21:37:00 +0100"),
-    'YEAR': 'tvi-tausen-fäimtåhn',
+    'SUBMIT_OPEN': parser.parse("2016-04-01 21:37:00 +0100"),
+    'YEAR': 'tvi-tausen-sääjstån',
     'MAX_NR_OF_MEMBERS': 10,
     'MAX_NR_OF_SITTNING': 146,
     'MAX_NR_OF_NEED_BED': 75,
     'MAX_NR_OF_NOT_SFS': 15,
-    'FACEBOOK_EVENT_URL': 'https://www.facebook.com/events/942839179066046/',
+    'FACEBOOK_EVENT_URL': 'https://www.facebook.com/events/580551922118139/',
     'TICKET_PRICE': 299,
     'TICKET_PRICE_WITH_SOVSAL': 399
 }
