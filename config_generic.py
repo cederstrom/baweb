663a43ade023d8b5daa895506ae326c5f4144696
Erik Horlin <erik.horlin@gmail.com>
2017-03-15 19:33:44 +0100

    Changed how schedule for flumride is handled
     - Now uses html templating from value set in config file.
       If schedule isn't set (or represented as empty array) a placeholder
       text will be displayed.
     - Added short description on how to format the schedule array in
       config_generic.py

--- config_generic.py
+++ config_generic.py
@@ -11,6 +11,21 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 FLUMRIDE = {
     'SUBMIT_OPEN': parser.parse("2017-03-21 03:00:00 +0100"),
     'YEAR': 'tvi-tausen-tjutåån',
+# EXAMPLE ON HOW TO FILL SCHEDULE DATA
+#    [
+#       {
+#           'day': 'Fredag',
+#           'data': [
+#           'Brutalare från alla världens hörn anländer till Karlskrona ungefär samtidigt som alla tävlande.',
+#           '12:00 - 18:00: Incheckning sker på kåren i Karlskrona. Här delas flumpass, partykit™ och lösa faddrar ut till alla anmälda.',
+#           '18:00 - 20:00: VM i caps på kåren som efterföljs av tokröj tills samtliga personer har stupat (d.v.s. spik kl 21:00).'
+#           ]
+#       },
+#       {
+#           'day': 'Lördag',
+#           'data': ['stuff', 'stuff2']
+#       }
+    'schedule': [],
     'MAX_NR_OF_MEMBERS': 10,
     'MAX_NR_OF_SITTNING': 146,
     'MAX_NR_OF_NEED_BED': 75,
