e575eefb56a0ed818222bf127e821d3d393999aa
Nikolas Eriksson <nikolasbajen@gmail.com>
2022-04-08 19:55:51 +0200

    Updated this years info

--- config_generic.py
+++ config_generic.py
@@ -10,11 +10,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 
 FLUMRIDE = {
-    'SUBMIT_OPEN': parser.parse("2019-04-01 18:00:00 +0100"),
-    'SUBMIT_CLOSE': parser.parse("2019-04-23 20:00:00 +0100"),
-    'YEAR': 'Tvi-tausen-Njeethåån',
-    'START_DATE': '3:e maj',
-    'END_DATE': '5:e maj',
+    'SUBMIT_OPEN': parser.parse("2022-04-09 12:00:00 +0100"),
+    'SUBMIT_CLOSE': parser.parse("2022-04-29 20:00:00 +0100"),
+    'YEAR': 'Tviitausen-tchjuh-tvauåh',
+    'START_DATE': '6:e maj',
+    'END_DATE': '8:e maj',
 # EXAMPLE ON HOW TO FILL SCHEDULE DATA
 #    [
 #       {
@@ -32,27 +32,33 @@ FLUMRIDE = {
     'schedule': [
         {
             'day': 'Fredag',
-            'data': ["Caps VM"]
+            'data': ['Brutalare från alla världens hörn anländer till Karlskrona ungefär samtidigt som alla tävlande.',
+           '12:00 - 17:00: Incheckning sker utanför Multisalen i Karlskrona. Här delas flumpass, partykit™ och det finns även chans till att köpa capshandukar.',
+           '16:00 - 17:30: Nu är det dags för en ny brutalspårrt? Slip`n`die vilket kommer äga rum precis utanför incheckningen, se till att ni har kompisar på röntgen.',
+           '19:00 - 21:00 Sker den Olympiska Fyllan, direkt importerat från de gamla grekerna för att sedan avsluta kvällen på kårhuset Villan.']
         },
         {
             'day': 'Lördag',
-            'data': ["Flumrundan", "Sittning"]
+            'data': ['12:00 - 15:00 Flumrundan, lunch serveras under tiden.',
+             '18:00 - 20:00 Sittningen som ni kanske inte kommer ihåg i efterhand.',
+             '20:00 - 21:00 ÖVERRASKNING',
+             '21:00 - 02:00 Kårkväll på Villan.']
         },
         {
             'day': 'Söndag',
-            'data': ["Nu är det dags att åka hem och börja återhämtningen till nästa år"]
+            'data': ["Nu är det dags att åka hem och börja återhämtningen till nästa år. Utcheckning sker senast 12:00."]
         }
     ],
     'ticket_types': [
         {
             'name': 'Kånntainerpasset',
-            'price': 349,
-            'description': 'Sovsal (fredag och lördag) inkl. frukost, sittning, flumrunda med spårrter och "lagom" mycket dônk (är detta året vi faktiskt ska dricka upp allt?), capsVM (inkl. öhl), lunch på lördag, ett fräckt märke, två kårkvällar',
+            'price': 399,
+            'description': 'Sovsal (fredag och lördag) inkl. frukost, sittning, flumrunda med spårrter och "lagom" mycket dônk (är detta året vi faktiskt ska dricka upp allt?), lunch på lördag, ett fräckt märke, två kårkvällar',
             'max_nr': 70
         },
         {
             'name': 'Kundvagnspasset',
-            'price': 249,
+            'price': 299,
             'description': 'Allt i kånntainerpasset minus sovsal och frukost',
             'max_nr': 60
         },
@@ -64,14 +70,14 @@ FLUMRIDE = {
         }
     ],
     'payment': {
-        'last_payment_date': '2019-04-29',
+        'last_payment_date': '2019-04-30',
         'bank': 'Swedbank',
-        'account_number': '8327-9,944 894 648-4'
+        'account_number': '8214,704 294 840'
     },
     'MAX_NR_OF_MEMBERS': 10,
-    'MAX_NR_OF_NOT_SFS': 70,
-    'FACEBOOK_EVENT_URL': 'https://www.facebook.com/events/399225683884908/',
-    'TRAILER_URL': 'https://www.youtube.com/embed/RgPMpL3jdkY'
+    'MAX_NR_OF_NOT_SFS': 60,
+    'FACEBOOK_EVENT_URL': 'https://www.facebook.com/events/842160829812821',
+    'TRAILER_URL': 'https://www.youtube.com/embed/r9AT5BzPg0Y'
 }
 
 FACEBOOK_GRAPH_URL = 'https://graph.facebook.com/'
