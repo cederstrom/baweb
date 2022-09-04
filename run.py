508f3d42373d8bd91f94cc12915061bb5e098bac
Nikolas Eriksson <nikolasbajen@gmail.com>
2022-09-04 12:13:48 +0200

    Test fix

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 from app import app
-app.run(debug=True)
+app.run(debug=True, ssl_context=('../cert.pem', '../key.pem'))
 
 #, ssl_context=('../cert.pem', '../key.pem') to run locally.
