d75ab8339210ac1a927bc6ea3a26e01aa1f2dcca
Nikolas Eriksson <nikolasbajen@gmail.com>
2023-01-13 12:36:54 +0100

    Removed () to support authentication

--- app/decorators.py
+++ app/decorators.py
@@ -5,7 +5,7 @@ from flask import abort, g
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwds):
-        if not (g.user.is_authenticated() and g.user.is_admin):
+        if not (g.user.is_authenticated and g.user.is_admin):
             abort(401)
         return f(*args, **kwds)
     return wrapper
