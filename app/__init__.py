95ce5dd1a7bc07f3abe4cd9abcc0daefbcb1c718
Nikolas Eriksson <nikolasbajen@gmail.com>
2023-07-04 21:39:17 +0200

    Implemented sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_session import Session
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.config.from_object('config')
-db = SQLAlchemy(app)
-lm = LoginManager(app)
 app.secret_key = app.config['SECRET_KEY']
-app.app_context().push()
+
+# Initialize SQLAlchemy and Session
+db = SQLAlchemy(app)
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+app.config['SESSION_SQLALCHEMY'] = db
+sess = Session(app)
+
+# Initialize LoginManager
+lm = LoginManager(app)
 
 from app import views, models
--- db_create.py
+++ db_create.py
@@ -1,14 +1,6 @@
+#Could not get the migrate stuff to work with sessions, another time perhaps.
 #!/usr/bin/env python
-from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
-from app import db
-import os.path
+from app import app, db
 
-
-db.create_all()
-if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
-                        api.version(SQLALCHEMY_MIGRATE_REPO))
+with app.app_context():
+    db.create_all()
