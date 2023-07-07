874be7b3db543759ea828dce3661920e09b85e9d
Nikolas Eriksson <nikolasbajen@gmail.com>
2023-07-07 09:44:12 +0200

    Reverted and fixed issues

--- db_create.py
+++ db_create.py
@@ -1,6 +1,12 @@
-#Could not get the migrate stuff to work with sessions, another time perhaps.
-#!/usr/bin/env python
-from app import app, db
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
 
-with app.app_context():
-    db.create_all()
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
+                        api.version(SQLALCHEMY_MIGRATE_REPO))
